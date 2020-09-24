846d18c487ebe4c42d74fd3fbf51cc7cfc170597
Charlie Hsieh <charlieofficial@gmail.com>
2020-09-24 16:39:24 +0800

    change to be more clear

--- lib/test_rails_gem.rb
+++ lib/test_rails_gem.rb
@@ -3,5 +3,19 @@ require "test_rails_gem/core_ext"
 require "test_rails_gem/acts_as_gem"
 
 module TestRailsGem
-  # Your code goes here...
+  # --- Your code goes here... ---
+  # Remember to comment out this above:
+  #     # require "test_rails_gem/acts_as_gem"
+  # --- Your code goes here... ---
+  #
+  # module ActsAsGem
+  #   extend ActiveSupport::Concern
+  #
+  #   class_methods do
+  #     def acts_as_gem(options = {})
+  #       # https://api.rubyonrails.org/classes/Module.html#method-i-mattr_accessor
+  #       cattr_accessor :gem_text_field, default: (options[:gem_text_field] || :last_squawk).to_s
+  #     end
+  #   end
+  # end
 end
--- lib/test_rails_gem/acts_as_gem.rb
+++ lib/test_rails_gem/acts_as_gem.rb
@@ -4,6 +4,7 @@ module TestRailsGem
 
     class_methods do
       def acts_as_gem(options = {})
+        # https://api.rubyonrails.org/classes/Module.html#method-i-mattr_accessor
         cattr_accessor :gem_text_field, default: (options[:gem_text_field] || :last_squawk).to_s
       end
     end
